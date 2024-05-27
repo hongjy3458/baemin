8b60c2d55d418f1dcb5fe64ca8fd13e3490babbe
DESKTOP-OSQ1V9F\user1 <wnstj1478@gmail.com>
2024-05-27 09:05:59 +0900

    가게허가

---
+++ src/main/java/com/kh/app/store/controller/StoreAdmitController.java
@@ -0,0 +1,11 @@
+package com.kh.app.store.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet
+public class StoreAdmitController extends HttpServlet {
+	
+	
+
+}
